6348de765d935f061769cedefd25e308d689cd8c
Łukasz <andzejziobrowski@gmail.com>
2021-10-04 17:30:52 +0200

    final heap sort

--- Heap.java
+++ Heap.java
@@ -2,6 +2,29 @@ package com.company;
 
 import java.util.Arrays;
 
+/*
+The binary Heap (Min and Max) data structure is an array object that we can view as a nearly complete binary tree.
+The important think is relation between children and parent. This relation should be (Parent >= Child).
+That's mean that first element in array will be the highest/smallest (Heap Max/ Heap Min) number in Heap.
+We can use this information to sort all array but first, we need to build a heap.
+
+To build a heap we can do this in two ways, by heapify or build by insert.
+Heapify works like, go to the last parent (size/2 - 1) and check relation between parent and children.
+if the parent is bigger/smaller (Min or Max) than the child, we need to exchange this two nodes.
+If it's ok than we can continue procedure by go to node (size/2 - 2) because every node from (size/2 - 1) to 0 is parent.
+
+Build by insert works like, I take a value, and I put this value at the end of heap. From the end I check every parent
+by climbing all the way to the root. On this way I check where I could put my value.
+I stop this procedure when (Parent > value / Parent < value).
+
+That's why it could be different results using this two methods.
+
+Sorting looks like, I take a first element from array, and I exchange them with the last one. I need to reduce array size by 1,
+and build a heap but for size = size - 1 and so on until size will be 1.
+
+I use static classes Min and MAx to clearly show, which heap I want to build.
+* */
+
 class Child {
 
     protected static int parent(int i) { return (i/2);}
@@ -25,8 +48,10 @@ abstract class ArrayOperations extends HeapArray {
 
     protected abstract boolean compareNodes(int idFirstNode, int idSecondNode);
 
+    private boolean arrayIsFull() {return (size == capacity);}
+
     protected void checkTheCapacity() {
-        if(size == capacity) {
+        if(arrayIsFull()) {
             capacity *= 2;
             heapArray = Arrays.copyOf(heapArray,capacity);
         }
@@ -66,6 +91,14 @@ abstract class ArrayOperations extends HeapArray {
 
 abstract class HeapProcedures extends ArrayOperations {
 
+    private boolean findIndex(int indexForNewElement, int key) {
+        return ((indexForNewElement > 0) && !compareNodes(heapArray[Child.parent(indexForNewElement)], key));
+    }
+
+    private boolean heapIsEmpty() { return (size == 0);}
+
+    private void restoreArraySize(int rememberSize) { size = rememberSize; }
+
     private void heapify(int subRoot) {
 
         int index = checkTheKids(subRoot);
@@ -82,7 +115,7 @@ abstract class HeapProcedures extends ArrayOperations {
         heapArray[size] = key;
         int indexForNewElement = size;
 
-        while ( (indexForNewElement > 0) && !compareNodes(heapArray[Child.parent(indexForNewElement)], key) ) {
+        while ( findIndex(indexForNewElement,key) ) {
             heapArray[indexForNewElement] = heapArray[Child.parent(indexForNewElement)];
             indexForNewElement = Child.parent(indexForNewElement);
         }
@@ -92,9 +125,8 @@ abstract class HeapProcedures extends ArrayOperations {
 
     }
 
-
     public void build() {
-        if(size == 0) throw new IllegalStateException("You didn't build a Heap or it's empty!");
+        if(heapIsEmpty()) throw new IllegalStateException("You didn't build a Heap or it's empty!");
 
         for (int i = (size/2) - 1; i >= 0; i--) heapify(i);
     }
@@ -122,11 +154,10 @@ abstract class HeapProcedures extends ArrayOperations {
             heapify(0);
         }
 
-        size = rememberSize;
+        restoreArraySize(rememberSize);
     }
 
 }
-
 
 public class Heap {
 
@@ -144,26 +175,6 @@ public class Heap {
         protected boolean compareNodes(int idFirstNode, int idSecondNode) {
             return (idFirstNode > idSecondNode);
         }
-    }
-
-    public static void main(String[] args) {
-        Heap.Max heap = new Max();
-
-        int[] a = {4,1,3,2,16,9,10,14,8,7};
-        heap.build(a);
-        heap.printHeap();
-//
-//        heap.insert(55);
-//        heap.printHeap();
-//
-//        heap.sort();
-//        heap.printHeap();
-//
-//        heap.insert(67);
-//        heap.build();
-//        heap.sort();
-//        heap.printHeap();
-
     }
 
 }
