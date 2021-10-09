5eee17cfbaf638aa9c7156f29c966aff2cde19b2
Łukasz <andzejziobrowski@gmail.com>
2021-10-09 17:37:06 +0200

    Bucket sort - modify

--- RationNumbers.java
+++ RationNumbers.java
@@ -1,8 +1,32 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
+
+/*
+Bucket sort divides the interval [0,1) into n equal-sized subintervals, or buckets,
+and then distributes the n input numbers into the buckets.
+Since the inputs are uniformly and independently distributed over [0,1),
+we do not expect many numbers to fall into each bucket. To produce the output,
+we simply sort the numbers in each bucket and then go through the buckets in order, listing the elements in each.
+
+             Bucket sort has an average-case running time of O(n).
+
+
+I modify this program (class RationNumbers) to sort not just numbers from interval [0,1) but every number greater than 0.
+For e.g.:
+            {1.78, 1.17, 7.77, 0.94, 0.78, 12.78, 12.17, 456.78, 456.17, 78.78, 78.17}
+
+First of all, I use Counting sort to sort numbers but sort by using this part before dot (Integer -> [12].78 )
+That's why the result will be like:
+            {0.94, 0.78, 1.78, 1.17, 7.77, 12.78, 12.17, 78.78, 78.17, 456.78, 456.17}
+
+Now, I can use Bucket sort to sort every sub-array (f.e.g: 0.94, 0.78 or 12.78, 12.17) but I will be sort just
+numbers by using part after dot. For e.g.:
+                    12.78, 12.17 take just second part -> 0.78, 0.17
+The results will be:
+             { 0.78, 0.94, 1.17, 1.78, 7.77, 12.17, 12.78, 78.17, 78.78, 456.17, 456.78}
+
+* */
 
 class ArrayOperationForCountingSort {
 
@@ -132,37 +156,32 @@ class Bucket {
 
 public class RationNumbers {
 
+    private static boolean condition(int index, int end) { return (index < end);}
+
     private static int getStartIndex(int j) {
         if(j != 0) return Counting.listOfIndex.get(j-1);
         else return 0;
     }
 
-    public static void sort(double[] array) {
+    private static void sortAndUpdateAnArray(double[] resultArray, int start, int end) {
+        double[] tempArray = Bucket.sort(Arrays.copyOfRange(resultArray,start,end));
+
+        for (int j = start, k = 0; condition(j,end) && condition(k, tempArray.length); j++, k++) {
+            resultArray[j] = tempArray[k];
+        }
+    }
+
+    public static double[] sort(double[] array) {
         double[] resultArray = Arrays.copyOf(Counting.sort(array),array.length);
 
         for (int i = 0; i < Counting.listOfIndex.size(); i++) {
-            int startIndex = getStartIndex(i), endIndex = Counting.listOfIndex.get(i);
+            int startIndex = getStartIndex(i);
+            int endIndex = Counting.listOfIndex.get(i);
 
-            double[] tempArray = Bucket.sort(Arrays.copyOfRange(resultArray,startIndex,endIndex));
-            for (int j = startIndex, k = 0; (j < endIndex) && (k < tempArray.length); j++, k++) {
-                resultArray[j] = tempArray[k];
-            }
+            sortAndUpdateAnArray(resultArray,startIndex,endIndex);
         }
 
-        System.out.println(Arrays.toString(resultArray));
+        return resultArray;
     }
-
-
-    public static void main(String[] args) {
-        double[] d = {0.78,0.17,0.39,0.26,0.72,0.94,0.21,0.12,0.23,0.68};
-        double[] test = {1.78,1.17,1.39,1.26,1.72,1.94,1.21,1.12,1.23,1.68,
-                        0.94,0.78,0.17,0.39,0.26,0.72,0.21,0.12,0.23,0.68,
-                        12.78,12.17,12.39,12.26,12.72,12.94,12.21,12.12,12.23,12.68,
-                        456.78,456.17,456.39,456.26,456.72,456.94,456.21,456.12,456.23,456.68,
-                        78.78,78.17,78.39,78.26,78.72,78.94,78.21,78.12,78.23,78.68};
-
-        RationNumbers.sort(test);
-
-    }
-
+    
 }
